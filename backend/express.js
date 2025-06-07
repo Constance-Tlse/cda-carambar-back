a494ac024ceb2d58bd6b97cf87fcebb4945e412f
Covarel <covarelcamille@gmail.com>
2025-06-07 20:39:37 +0200

    patch: package.json && express.js

--- backend/express.js
+++ backend/express.js
@@ -6,7 +6,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./swagger.js'); // Assurez-vous que ce fichier s'appelle bien swagger.js
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
