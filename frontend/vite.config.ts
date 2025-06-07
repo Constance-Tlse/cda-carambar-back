9775056f72835a0695862239ab4099ba3b364945
Covarel <covarelcamille@gmail.com>
2025-06-08 01:30:28 +0200

    patch: front-end

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: './',
+    base: 'https://constance-tlse.github.io/cda-carambar-back/',
     plugins: [react()],
     build: {
         outDir: 'dist',
